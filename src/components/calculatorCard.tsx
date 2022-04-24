861a15d89122b1d05c6834065888338346b119ef
Rofiat <rofiat@bptn.com>
2022-04-24 11:21:20 -0400

    Created a component folder

---
+++ src/components/calculatorCard.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Paper from '@mui/material/Paper';
+
+function calculatorCard(){
+    return <Paper elevation={3} square> Hello </Paper>;
+};
+
+export default calculatorCard;
