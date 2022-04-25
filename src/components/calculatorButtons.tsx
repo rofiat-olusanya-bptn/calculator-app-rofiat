13100a7a13ddff31cdb91dac7e9fe733c842bf35
Rofiat <rofiat@bptn.com>
2022-04-25 14:17:02 -0400

    removed styled components and added material ui button

--- src/components/calculatorButtons.tsx
+++ src/components/calculatorButtons.tsx
@@ -1,22 +1,35 @@
-import React from "react";
+import React, {FunctionComponent} from "react";
 import Grid from '@mui/material/Grid';
 import Styled from 'styled-components';
+import Button from '@mui/material/Button';
 
-const StyledButton = Styled.button`
-background-color: #f90os7;
-border-radius: 8px;
-border: none;
-`;
+// const StyledButton = Styled(Button)`
+// background-color: #FF0000;
+// border-radius: 50px;
+// border: none;
+// padding: 10px;
+// font-size: 400px;
+// `;
 
-type buttonProps = {
-    label: string;
+
+
+interface buttonProps {
+    label: string,
+    styleType?: string,
 }
 
-function calculatorButtons(props: buttonProps){
+const calculatorButtons:FunctionComponent<buttonProps> = ({label, styleType}) => {
+    if (styleType === "big"){
+       return (<Button variant="contained" color="success">
+            {label}
+        </Button>)
+    }
     return(
-            <StyledButton>
-                {props.label}
-            </StyledButton>
+
+        //variant="contained" color={styleType === "big" ? "success" : undefined}
+        <Button >
+            {label}
+        </Button>    
     )
 }
 
