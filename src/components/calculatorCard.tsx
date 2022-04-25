175a9a964c05c34616f0e3a554ca921081705243
Rofiat <rofiat@bptn.com>
2022-04-25 14:18:09 -0400

    Added display component to card

--- src/components/calculatorCard.tsx
+++ src/components/calculatorCard.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import Paper from '@mui/material/Paper';
 import Styled from 'styled-components';
 import CalculatorButtons from "./calculatorButtons";
+import CalculatorDisplay from "./calculatorDisplay";
 import Box from '@mui/material/Box';
+import { lightBlue } from "@mui/material/colors";
 
 const Grid = Styled.div`
 display: grid;
@@ -18,27 +20,43 @@ function calculatorCard(){
         display: 'flex',
         justifyContent: "center",
         alignItems: "center",
+        margin: 15,
+        background: lightBlue,
         '& > :not(style)': {
           m: 1,
           width: 400,
-          height: 200,
+          height: 400,
           padding: 1,
         },
       }}
     >
-    <Paper elevation={3} square> 
+    <Paper elevation={3}> 
         <Grid>
-          <CalculatorButtons label="9" />
-          <CalculatorButtons label="8"/>
+          <CalculatorDisplay />
+          <CalculatorButtons label="C" />
+          <CalculatorButtons label="("/>
+          <CalculatorButtons label=")"/>
+          <CalculatorButtons label="x"/>
+          <CalculatorButtons label="√" />
+          <CalculatorButtons label="%"/>
+          <CalculatorButtons label="±" />
+          <CalculatorButtons label="÷"/>
           <CalculatorButtons label="7"/>
-          <CalculatorButtons label="6"/>
-          <CalculatorButtons label="5"/>
+          <CalculatorButtons label="8"/>
+          <CalculatorButtons label="9"/>
+          <CalculatorButtons label="-"/>
           <CalculatorButtons label="4"/>
-          <CalculatorButtons label="3"/>
-          <CalculatorButtons label="2"/>
+          <CalculatorButtons label="5"/>
+          <CalculatorButtons label="6"/>
+          <CalculatorButtons label="+"/>
           <CalculatorButtons label="1"/>
-          <CalculatorButtons label="0"/>
+          <CalculatorButtons label="2"/>
+          <CalculatorButtons styleType="big" label="3"/>
+          <CalculatorButtons label="="/>
           <CalculatorButtons label="."/>
+          <CalculatorButtons label="0"/>
+          <CalculatorButtons label="⌫" />
+          
         </Grid>
     </Paper>
     </Box>
