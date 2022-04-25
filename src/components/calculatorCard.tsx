f3770fab3f4b0ee22e24194d25cd2002ff0179d6
Rofiat <rofiat@bptn.com>
2022-04-25 18:50:40 -0400

    removed unused buttons

--- src/components/calculatorCard.tsx
+++ src/components/calculatorCard.tsx
@@ -20,43 +20,39 @@ function calculatorCard(){
         display: 'flex',
         justifyContent: "center",
         alignItems: "center",
-        margin: 15,
-        background: lightBlue,
+        margin: 5,
         '& > :not(style)': {
           m: 1,
           width: 400,
-          height: 400,
-          padding: 1,
+          height: 450,
+          py: 3,
+          px: 3,
         },
       }}
     >
     <Paper elevation={3}> 
         <Grid>
           <CalculatorDisplay />
-          <CalculatorButtons label="C" />
-          <CalculatorButtons label="("/>
-          <CalculatorButtons label=")"/>
-          <CalculatorButtons label="x"/>
-          <CalculatorButtons label="√" />
-          <CalculatorButtons label="%"/>
-          <CalculatorButtons label="±" />
-          <CalculatorButtons label="÷"/>
+          <CalculatorButtons styleType="c" label="C" />
+          <CalculatorButtons styleType="other" label="("/>
+          <CalculatorButtons styleType="other" label=")"/>
+          <CalculatorButtons styleType="other" label="x"/>
           <CalculatorButtons label="7"/>
           <CalculatorButtons label="8"/>
           <CalculatorButtons label="9"/>
-          <CalculatorButtons label="-"/>
+          <CalculatorButtons styleType="other" label="÷"/>
           <CalculatorButtons label="4"/>
           <CalculatorButtons label="5"/>
           <CalculatorButtons label="6"/>
-          <CalculatorButtons label="+"/>
+          <CalculatorButtons styleType="other" label="-"/>
           <CalculatorButtons label="1"/>
           <CalculatorButtons label="2"/>
-          <CalculatorButtons styleType="big" label="3"/>
-          <CalculatorButtons label="="/>
+          <CalculatorButtons  label="3"/>
+          <CalculatorButtons styleType="other" label="+"/>
           <CalculatorButtons label="."/>
           <CalculatorButtons label="0"/>
           <CalculatorButtons label="⌫" />
-          
+          <CalculatorButtons styleType="big" label="="/>
         </Grid>
     </Paper>
     </Box>
