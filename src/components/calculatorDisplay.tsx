09641846bc0f20e54e005ddee92d62ab8bc32fc5
Rofiat <rofiat@bptn.com>
2022-04-25 18:50:02 -0400

    Added a margin-bottom to display

--- src/components/calculatorDisplay.tsx
+++ src/components/calculatorDisplay.tsx
@@ -3,14 +3,14 @@ import styled from "styled-components";
 
 
 const Display = styled.div `
-background: #F93822ff;
+background: #f3f1f3;
 border-radius: 8px;
 font-size: 48px;
 grid-column-end: span 4;
 text-align: right;
 height: 120px;
-margin-left: 15px;
 padding: 30px 24px;
+margin-bottom: 10px;
 `;
 
 const calculatorDisplay = () =>{
