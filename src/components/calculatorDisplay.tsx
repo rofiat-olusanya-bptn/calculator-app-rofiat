e15f20f7acd717dddafe25bc3cb055d61f76d31a
Rofiat <rofiat@bptn.com>
2022-04-26 21:52:26 -0400

    Added a prop that allows to send number to display

--- src/components/calculatorDisplay.tsx
+++ src/components/calculatorDisplay.tsx
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { FunctionComponent} from "react";
 import styled from "styled-components";
 
-
-const Display = styled.div `
-background: #f3f1f3;
-border-radius: 8px;
-font-size: 48px;
-grid-column-end: span 4;
-text-align: right;
-height: 120px;
-padding: 30px 24px;
-margin-bottom: 10px;
+const Display = styled.div`
+  background: #f3f1f3;
+  border-radius: 8px;
+  font-size: 48px;
+  grid-column-end: span 4;
+  text-align: right;
+  height: 120px;
+  padding: 30px 24px;
+  margin-bottom: 10px;
 `;
 
-const calculatorDisplay = () =>{
-    return(
-        <Display> 42 </Display>
-    )
+interface display {
+  label: any;
 }
 
-export default calculatorDisplay;
+const CalculatorDisplay: FunctionComponent<display> = ({label}) => {
+
+  return <Display> {label} </Display>;
+};
+
+export default CalculatorDisplay;
