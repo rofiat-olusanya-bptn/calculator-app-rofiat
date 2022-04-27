eb0c4134ccf21efdd4677f264bacd7efd79f3df0
Rofiat <rofiat@bptn.com>
2022-04-26 21:54:49 -0400

    Added react button props to MUI button>

--- src/components/calculatorButtons.tsx
+++ src/components/calculatorButtons.tsx
@@ -1,91 +1,90 @@
-import React, {FunctionComponent} from "react";
-import Button from '@mui/material/Button';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { FunctionComponent } from "react";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
-    palette: {
-      neutral: {
-        main: '#a9a9a9',
-        contrastText: '#fff',
-      },
-     primary:{
-        main: '#BE302B',
-        contrastText: '#fff'
+  palette: {
+    neutral: {
+      main: "#a9a9a9",
+      contrastText: "#fff",
     },
-    secondary:{
-        main:'#f07995',
-        contrastText: '#fff'
+    primary: {
+      main: "#BE302B",
+      contrastText: "#fff",
     },
-    info:{
-        main: '#Be3B45',
-        contrastText: '#fff'
-    }
+    secondary: {
+      main: "#f07995",
+      contrastText: "#fff",
     },
-  });
-  
-  declare module '@mui/material/styles' {
-    interface Palette {
-      neutral: Palette['primary'];
-    }
-  
-    // allow configuration using `createTheme`
-    interface PaletteOptions {
-      neutral?: PaletteOptions['primary'];
-    }
-  }
-  
-  // Update the Button's color prop options
-  declare module '@mui/material/Button' {
-    interface ButtonPropsColorOverrides {
-      neutral: true;
-    }
+    info: {
+      main: "#Be3B45",
+      contrastText: "#fff",
+    },
+  },
+});
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    neutral: Palette["primary"];
   }
 
-interface buttonProps {
-    label: string,
-    styleType?: string,
+  // allow configuration using `createTheme`
+  interface PaletteOptions {
+    neutral?: PaletteOptions["primary"];
+  }
 }
 
-const calculatorButtons:FunctionComponent<buttonProps> = ({label, styleType}) => {
-    if (styleType === "big"){
-       return (
-        <ThemeProvider theme={theme}>
-       <Button variant="contained" color="primary">
-            {label}
+// Update the Button's color prop options
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    neutral: true;
+  }
+}
+
+type buttonProps = React.HTMLProps<HTMLButtonElement> & {
+  label: string;
+  styleType?: string;
+}
+
+const calculatorButtons: FunctionComponent<buttonProps> = ({
+  label,
+  styleType,
+  onClick,
+}) => {
+  if (styleType === "big") {
+    return (
+      <ThemeProvider  theme={theme}>
+        <Button onClick={onClick} variant="contained" color="primary">
+          {label}
         </Button>
-        </ThemeProvider>
-        
-        )
-    }
-    else if(styleType === "c"){
-        return (
-            <ThemeProvider theme={theme}>
-           <Button variant="contained" color="info">
-                {label}
-            </Button>
-            </ThemeProvider>
-            
-            )
-    }
-    else if(styleType === "other"){
-        return (
-            <ThemeProvider theme={theme}>
-           <Button variant="contained" color="secondary">
-                {label}
-            </Button>
-            </ThemeProvider>
-            
-            )
-    }
-    return(
-        //variant="contained" color={styleType === "big" ? "success" : undefined}
+      </ThemeProvider>
+    );
+  } else if (styleType === "c") {
+    return (
+      <ThemeProvider theme={theme}>
+        <Button onClick={onClick} variant="contained" color="info">
+          {label}
+        </Button>
+      </ThemeProvider>
+    );
+  } else if (styleType === "other") {
+    return (
+      <ThemeProvider theme={theme}>
+        <Button onClick={onClick} variant="contained" color="secondary">
+          {label}
+        </Button>
+      </ThemeProvider>
+    );
+  }
+  return (
+    //variant="contained" color={styleType === "big" ? "success" : undefined}
 
-        <ThemeProvider theme={theme}>
-        <Button variant="contained"  color="neutral">
-            {label}
-        </Button>   
-        </ThemeProvider> 
-    )
-}
+    <ThemeProvider theme={theme}>
+      <Button onClick={onClick} variant="contained" color="neutral">
+        {label}
+      </Button>
+    </ThemeProvider>
+  );
+};
 
-export default calculatorButtons
+export default calculatorButtons;
