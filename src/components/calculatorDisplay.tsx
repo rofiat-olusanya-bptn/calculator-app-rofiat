18893c2cc694098cb35d2fa181b1901a6f83eb50
Rofiat <rofiat@bptn.com>
2022-04-30 20:51:50 -0400

    Added functionality for calculator buttons

--- src/components/calculatorDisplay.tsx
+++ src/components/calculatorDisplay.tsx
@@ -1,24 +1,34 @@
+import { padding } from "@mui/system";
 import React, { FunctionComponent} from "react";
 import styled from "styled-components";
 
 const Display = styled.div`
-  background: #f3f1f3;
   border-radius: 8px;
   font-size: 48px;
   grid-column-end: span 4;
+  color: #B0B8B4FF;
   text-align: right;
   height: 120px;
   padding: 30px 24px;
-  margin-bottom: 10px;
+  margin: 30px 20px;
+  overflow: auto;
 `;
 
 interface display {
   label: any;
+  theme: any
 }
+const CalculatorDisplay: FunctionComponent<display> = ({label, theme}) => {
 
-const CalculatorDisplay: FunctionComponent<display> = ({label}) => {
-
-  return <Display> {label} </Display>;
+  return (
+  <Display style={{
+    "backgroundColor": `${theme?.palette?.mode === "dark" ? "black": '#FC766AFF'}`,
+     
+    }}>
+     {label} 
+  </Display>
+  
+  );
 };
 
 export default CalculatorDisplay;
