90a2150374d25e45bcb26e864b930cecdb9d89e6
Rofiat <rofiat@bptn.com>
2022-04-30 20:50:10 -0400

    Added a route to 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
-import CalculatorCard from './components/calculatorCard';
-import Container from '@mui/material/Container';
+import React, { useState } from 'react';
+import CalculatorCard from "./components/CalculatorCard"
 import './App.css';
+import { Container } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import NotFound from './components/NotFound';
 
 function App() {
+  
+
+  
+
   return (
-    <div className="App">
+    <div>
+    <Router>
       <Container>
-      <CalculatorCard />
+        <Routes>
+          <Route path="/" element={<CalculatorCard />} />
+          <Route path="*" element={<NotFound/>} />
+      </Routes>
       </Container>
+
+    </Router>
     </div>
   );
 }
 
+// const RouterPage = (
+//   props: { pageComponent: JSX.Element } & RouteComponentProps
+// ) => props.pageComponent;
+
+// <Route path="/*" element={NotFound} />
+
 export default App;
+
+
